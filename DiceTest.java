4d717db62cd18efa8a08d6567e00ccf978671caf
zhouyuxiao89 <zhouyuxiao89@yahoo.com>
2013-05-06 22:04:20 -0700

    Update DiceTest.java

--- DiceTest.java
+++ DiceTest.java
@@ -3,22 +3,19 @@ public class DiceTest {
   
     public static void main(String[] args) {
         	Scanner input = new Scanner(System.in);
-            	Dice dice1 = new Dice();
-                	Dice dice2 = new Dice();
-                    	//System.out.println("How many dices do want to roll?\n");
-                        	//int numberOfDice1 = input.nextInt();
-                            	//System.out.println("What is the max value of each dice?\n");
-                                	//int maxValue1 = input.nextInt();
-                                    	Dice.RollDice(2,10);
-                                        	System.out.printf("The times the dice1 rolled for static method is %d\n\n",dice1.RollCount1());
+          Dice dice1 = new Dice();
+        	Dice dice2 = new Dice();
+                    
+          Dice.RollDice(2,10);
+          System.out.printf("The times the dice1 rolled for static method is %d\n\n",dice1.RollCount1());
                                             
-                                              	// =========================================
-                                                  	dice2.Roll_1_Dice(); 
-                                                      	dice2.Roll_1_Dice();
-                                                          	dice2.Roll_1_Dice(); 
-                                                              	dice2.Roll_1_Dice();
-                                                                  	Dice.RollDice(2,10);
-                                                                      	System.out.printf("The times the dice2 rolled for non-static method is %d\n\n",dice2.RollCount2());
-                                                                          	System.out.printf("The times the dice1 rolled for static method is %d\n\n",dice1.RollCount1());
+          // =========================================
+          dice2.Roll_1_Dice(); 
+        	dice2.Roll_1_Dice();
+          dice2.Roll_1_Dice(); 
+         	dice2.Roll_1_Dice();
+          Dice.RollDice(2,10);
+          System.out.printf("The times the dice2 rolled for non-static method is %d\n\n",dice2.RollCount2());
+          System.out.printf("The times the dice1 rolled for static method is %d\n\n",dice1.RollCount1());
     }
 }
